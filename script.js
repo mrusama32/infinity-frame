eb8c1f34a0a549f730baf75d8117de3c65bc164d
Muhammad Usama <usamachauhan876@gmail.com>
2026-07-07 11:39:14 +0500

    Version 2.4

--- script.js
+++ script.js
@@ -15,4 +15,58 @@ document.querySelectorAll('nav a').forEach(link => {
             });
         }
     });
-});
+});// Typing Animation
+
+const text = [
+    "Front-End Web Developer",
+    "Digital Creator",
+    "Founder of Infinity Frame",
+    "Freelancer"
+];
+
+let textIndex = 0;
+let charIndex = 0;
+
+const typing = document.getElementById("typing");
+
+function type() {
+
+    if (charIndex < text[textIndex].length) {
+
+        typing.textContent += text[textIndex].charAt(charIndex);
+        charIndex++;
+
+        setTimeout(type, 100);
+
+    } else {
+
+        setTimeout(erase, 1500);
+
+    }
+
+}
+
+function erase() {
+
+    if (charIndex > 0) {
+
+        typing.textContent = text[textIndex].substring(0, charIndex - 1);
+        charIndex--;
+
+        setTimeout(erase, 50);
+
+    } else {
+
+        textIndex++;
+
+        if (textIndex >= text.length) {
+            textIndex = 0;
+        }
+
+        setTimeout(type, 300);
+
+    }
+
+}
+
+type();
